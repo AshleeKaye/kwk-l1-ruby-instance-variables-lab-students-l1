a8bb009746bfbcac0632111a1da74ce7a3d3416d
AshleeKaye <ashleemk@hotmail.com>
2018-06-07 23:28:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # dog.rb
+class Dog
+  
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  def name
+    @this_dogs_name
+end
+
+  dogs_name = Dog.new
+  dogs_name = "Harry & Dolce"
+  puts "My dogs names are #{dogs_name}"
+end
